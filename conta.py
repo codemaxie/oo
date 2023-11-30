ae64656fc39ba9747365d996e714a84cfdefdd22
Your Name <saiyadin00@gmail.com>
2023-11-30 02:43:48 -0300

    .

--- conta.py
+++ conta.py
@@ -1,18 +1,26 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
 
     def extrato(self):
-        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def sacar (self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere(self, valor,destino):
+        self.sacar(valor)
+        destino.deposita(valor)
+
+
+
+
